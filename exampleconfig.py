2f1b8680b5880b82e1e37461976cf9af97908443
Ashqalsmt <128143959+Ashqalsmt@users.noreply.github.com>
2025-08-19 21:28:06 +0300

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,15 +1,19 @@
 from sample_config import Config
-
-
 class Development(Config):
-    # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي اللي بين " " فقـط وضع اكـوادك
-    APP_ID = 6 #ضع كود الايبي ايدي الصغيـر مكان الرقم 6 نسخ لصق   
-    API_HASH = "ضع كود الايبي هاش"
-    ALIVE_NAME = "اسم حسابك التلي"
-    DB_URI = "رابـط التخـزين الخـاص بك"
-    STRING_SESSION = "كود تيرمــكس"
-    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
-    PRIVATE_GROUP_BOT_API_ID = -100  # ضـع ايـدي كـروب السجـل بجـانب العـدد -100 لا تمسـح العـدد هـذا
-    COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
-    SUDO_COMMAND_HAND_LER = "."  # اتركهــا كمـا هـي
-    TZ = "Asia/Baghdad"  # اتركهــا كمـا هـي
+    # get this values from the my.telegram.org
+    APP_ID = خلي ايبي ايدي هنا
+    API_HASH = "الايبي هاش"
+    # the name to display in your alive message
+    ALIVE_NAME = "اسمك ع التليجرام"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "رابط قاعدة البيانات"
+    # After cloning the repo and installing requirements do python3 telesetup.py an fill that value with this
+    STRING_SESSION = "كود التيرمكس"
+    # create a new bot in @botfather and fill the following vales with bottoken and username respectively
+    TG_BOT_TOKEN = "توكن البوت"
+    # command handler
+    COMMAND_HAND_LER = "."
+    # sudo enter the id of sudo users userid's in that array
+    SUDO_USERS = []
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
